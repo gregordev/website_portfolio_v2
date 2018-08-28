0c9667c571ec49505be0c1ea598a2988e79e91f1
gregordev <grzegorzwalczak94@gmail.com>
2018-08-29 01:17:55 +0200

    added fullscreen overlay effect when hit toggle button

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("main-nav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("main-nav").style.width = "0%";
+}
